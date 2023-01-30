a5425c155af39c96a3ae91f449475be9dbad444f
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2023-01-30 18:18:42 +0300

    code refactor

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,54 +1,10 @@
-import cors from 'cors';
-import express from 'express';
 import logger from 'logger';
-import pino from 'pino-http';
-import routes from './routes';
+import { env } from './env/server';
+import createServer from './server';
 
-const app = express();
-app.use(express.json());
-app.use(
-  cors({
-    origin: ['http://localhost:3000'],
-  })
-);
-app.use(
-  pino({
-    logger,
-    // Define a custom logger level
-    customLogLevel(req, res, err) {
-      if (res.statusCode >= 400 && res.statusCode < 500) {
-        return 'warn';
-      }
-      if (res.statusCode >= 500 || err) {
-        return 'error';
-      }
-      if (res.statusCode >= 300 && res.statusCode < 400) {
-        return 'silent';
-      }
-      return 'info';
-    },
-    // Define a custom success message
-    customSuccessMessage(req, res) {
-      if (res.statusCode === 404) {
-        return 'resource not found';
-      }
-      return `${req.method} completed`;
-    },
-    // Define a custom receive message
-    customReceivedMessage(req, res) {
-      return `request received: ${req.method}`;
-    },
-    // Define a custom error message
-    customErrorMessage(req, res, err) {
-      return `request errored with status code: ${res.statusCode}`;
-    },
-  })
-);
+const PORT = env.API_PORT || 3333;
+const server = createServer();
 
-app.use('/api/v1/', routes);
-
-const PORT = process.env.API_PORT || 3333;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server ready at: http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  logger.info(`🚀 Server ready at: http://localhost:${PORT}`);
 });
---
+++ apps/api/src/server.ts
@@ -0,0 +1,56 @@
+import cors from 'cors';
+import express, { type Express } from 'express';
+import logger from 'logger';
+import pino from 'pino-http';
+import routes from './routes';
+
+const createServer: () => Express = () => {
+  const app = express();
+  app
+    .disable('x-powered-by')
+    .use(express.json())
+    .use(
+      cors({
+        origin: ['http://localhost:3000'],
+      })
+    )
+    .use(
+      pino({
+        logger,
+        // Define a custom logger level
+        customLogLevel(req, res, err) {
+          if (res.statusCode >= 400 && res.statusCode < 500) {
+            return 'warn';
+          }
+          if (res.statusCode >= 500 || err) {
+            return 'error';
+          }
+          if (res.statusCode >= 300 && res.statusCode < 400) {
+            return 'silent';
+          }
+          return 'info';
+        },
+        // Define a custom success message
+        customSuccessMessage(req, res) {
+          if (res.statusCode === 404) {
+            return 'resource not found';
+          }
+          return `${req.method} completed`;
+        },
+        // Define a custom receive message
+        customReceivedMessage(req, res) {
+          return `request received: ${req.method}`;
+        },
+        // Define a custom error message
+        customErrorMessage(req, res, err) {
+          return `request errored with status code: ${res.statusCode}`;
+        },
+      })
+    )
+    // Routes
+    .use('/api/v1/', routes);
+
+  return app;
+};
+
+export default createServer;

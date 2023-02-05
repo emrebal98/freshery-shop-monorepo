36632e1f0001dcba201242a7d59f3893e1d6c436
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2023-02-05 20:39:53 +0300

    fix token generate

--- apps/api/src/utils/generateToken.ts
+++ apps/api/src/utils/generateToken.ts
@@ -5,11 +5,11 @@ import type { Payload } from '../types/auth';
 const generateToken = (payload: Payload, type?: 'refresh') => {
   if (type === 'refresh') {
     return jwt.sign(payload, env.JWT_SECRET, {
-      expiresIn: env.JWT_REFRESH_TOKEN_EXPIRES_IN,
+      expiresIn: Number(env.JWT_REFRESH_TOKEN_EXPIRES_IN),
     });
   }
   return jwt.sign(payload, env.JWT_SECRET, {
-    expiresIn: env.JWT_TOKEN_EXPIRES_IN,
+    expiresIn: Number(env.JWT_TOKEN_EXPIRES_IN),
   });
 };
 
